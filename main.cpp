2e4dae6dc30dd23d07a214798eee6e752cfd4eb6
Justin <jyang772@gmail.com>
2013-11-26 02:26:53 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
-#include <vector>
 #include <fstream>
 
 using namespace std;
-
+void function(string);
+void Start_Steam();
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -22,7 +22,7 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 	return size * nmemb;
 }
 
-void function(string);
+
 
 int main(void)
 {
@@ -52,47 +52,52 @@ void function(string line)
 	CURLcode res;
 	size_t found;
 	std::string contents;
+	size_t position, space;
+	string ip;
 
-curl = curl_easy_init();
-if (curl) {
+	curl = curl_easy_init();
+	if (curl) {
 
-	char *result = strdup(line.c_str());
-
-
-	curl_easy_setopt(curl, CURLOPT_URL, result);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &contents);
-	res = curl_easy_perform(curl);
-	curl_easy_cleanup(curl);
+		char *result = strdup(line.c_str());
 
 
+		curl_easy_setopt(curl, CURLOPT_URL, result);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &contents);  // sets user arguments
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
 
-	string status[3] = { "Currently Online", "Currently In-Game", "Offline" };
-	for (int i = 0; i < 3; i++)
-	{
-		found = contents.find(status[i]);
-		if (found != std::string::npos){
-			cout << status[i] << endl;
+
+
+		string status[3] = { "Currently Online", "Currently In-Game", "Offline" };
+		for (int i = 0; i < 3; i++)
+		{
+			found = contents.find(status[i]);
+			if (found != std::string::npos){    //until end of string
+				cout << status[i] << endl;
+
+				if (i == 1)
+				{
+
+					cout << "YAYA!" << endl;
+					found = contents.find("steam://connect/");         //finds position of this text
+					cout << found << endl; 
+					ip = contents.substr(found+16, contents.length()-1);        //ip is set to contain text "steam" to end of file
+					position = ip.find(" ");                                 //sets position to end of IP address
+					ip = contents.substr(found+16, position-1);                    // sets "ip" to whole line
+					cout << ip << endl;
+
+					ofstream batch;
+					batch.open("Launch.bat");
+					batch << "@echo\n" << "\"C:\\Program Files (x86)\\Steam\\steam.exe\" -applaunch 440 +connect " << ip << endl;
+					batch.close();
+					system("Launch.bat");
+			}
+
+		}
 		}
 	}
-	/**
-	found = contents.find("Currently Online");
-	if (found != std::string::npos){
-		cout << "Currently Online!" << endl;
-	}
-	found = contents.find("Currently In-Game");
-	if (found != std::string::npos){
-		cout << "Currently In-Game!" << endl;
-	}
-	found = contents.find("Offline");
-	if (found != std::string::npos){
-		cout << "Currently Offline!" << endl;
-	}
-	else
-		cout << "NAH!!!" << endl;
-	**/
+}
 
 
-}
-}
 
