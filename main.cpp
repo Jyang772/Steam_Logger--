14ba8837e1beb4c2ebea2acaa43a24dfb1475a93
Justin <jyang772@gmail.com>
2013-11-26 00:11:51 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,63 +1,98 @@
+// 
+// curltest.cpp 
+//
+
+
+#pragma warning (disable : 4996)
+
+
+
 #include <iostream>
 #include <string>
-#include <fsttream>
+#include <curl/curl.h>
+#include <vector>
+#include <fstream>
+
 using namespace std;
 
 
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
+}
 
+void function(string);
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	string line;
+	char* troll;
 	ifstream myfile("profiles.txt");
 	if (myfile.is_open())
 	{
 		while (getline(myfile, line))
-		{ 
-			Get_Html(line);
+		{
+			cout << line << endl;
+			function(line);
 		}
 		myfile.close();
 	}
-	
+
+
 	system("PAUSE");
+
 	return 0;
-
 }
 
-void Get_Html(string link)
+void function(string line)
 {
+
+	CURL *curl;
+	CURLcode res;
 	size_t found;
-	CURL* curl = curl_easy_init();
-	if (curl)
+	std::string contents;
+
+curl = curl_easy_init();
+if (curl) {
+
+	char *result = strdup(line.c_str());
+
+
+	curl_easy_setopt(curl, CURLOPT_URL, result);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &contents);
+	res = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
+
+
+
+	string status[3] = { "Currently Online", "Currently In-Game", "Offline" };
+	for (int i = 0; i < 3; i++)
 	{
-		// Tell libcurl the URL 
-		curl_easy_setopt(curl, CURLOPT_URL, link);
-		// Tell libcurl what function to call when it has data 
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,);
-		// Do it! 
-		CURLcode res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-
-		if (res == 0)
-		{
-			found = contents.find("Currently Online");
-			if (found != std::string::npos){
-				cout << "Currently Online!" << endl;
-			}
-			found = contents.find("Currently In-Game");
-			if (found != std::string::npos){
-				cout << "Currently In-Game!" << endl;
-			}
-			found = contents.find("Offline");
-			if (found != std::string::npos){
-				cout << "Currently Offline!" << endl;
-			}
-			else
-				cout << "NAH!!!" << endl;
+		found = contents.find(status[i]);
+		if (found != std::string::npos){
+			cout << status[i] << endl;
 		}
-
-		else
-			cerr << "Error: " << res << endl;
 	}
+	/**
+	found = contents.find("Currently Online");
+	if (found != std::string::npos){
+		cout << "Currently Online!" << endl;
+	}
+	found = contents.find("Currently In-Game");
+	if (found != std::string::npos){
+		cout << "Currently In-Game!" << endl;
+	}
+	found = contents.find("Offline");
+	if (found != std::string::npos){
+		cout << "Currently Offline!" << endl;
+	}
+	else
+		cout << "NAH!!!" << endl;
+	**/
+
 
 }
+}
+
